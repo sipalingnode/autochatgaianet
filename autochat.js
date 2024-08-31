d16be2cfdd7050ab1efa31a20a07794022df2fb8
Node Army <109174478+sipalingnode@users.noreply.github.com>
2024-09-01 01:32:27 +0700

    Add files via upload

---
+++ autochat.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+const fs = require('fs');
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+;(async () => {
+    try {
+        console.log('AUTOCHAT GAIANET BY AIRDROP ASC\n\n');
+        const addressList = await fs.readFileSync('chat.txt', 'utf-8');
+        const addressListArray = await addressList.split('\n');
+
+        for (let index = 11; index < addressListArray.length; index++) {
+            const Wallet = addressListArray[index];
+            console.log("Content Chat: " + Wallet + "\n");
+
+            try {
+                const response = await axios.post(
+                    'https://nodeidmu.us.gaianet.network/v1/chat/completions',
+                    {
+                        'messages': [
+                            {
+                                'role': 'system',
+                                'content': 'You are a helpful assistant.'
+                            },
+                            {
+                                'role': 'user',
+                                'content': `${Wallet}`
+                            }
+                        ]
+                    },
+                    {
+                        headers: {
+                            'accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                );
+
+                console.log("Response: [" + response.data.choices[0].message.content + "]\n");
+                console.log("WAIT 30 DETIK \n\n");
+                await delay(30000);
+            } catch (postError) {
+                console.error("Error during axios post: ", postError);
+            }
+        }
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+})();
